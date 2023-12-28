ac952fb28b3cbb11eda45aaae82222fc02744955
gajendrarajoriya91 <gajendraprofe987@gmail.com>
2023-12-28 21:46:52 +0530

    handle latest stories data dynamically

--- server.js
+++ server.js
@@ -1,22 +1,70 @@
 const http = require("http");
-const fs = require("fs");
+const https = require("https");
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
+const fetchLatestStories = () => {
+  return new Promise((resolve, reject) => {
+    const options = {
+      hostname: "time.com",
+      path: "/",
+      method: "GET",
+    };
+
+    const req = https.request(options, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        const latestStories = extractLatestStories(data);
+
+        if (latestStories) {
+          resolve(latestStories);
+        } else {
+          reject("Unable to extract latest stories.");
+        }
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
+};
+
+const extractLatestStories = (html) => {
+  const latestStories = [];
+  const storyPattern =
+    /<li class="latest-stories__item">.*?<a href="(.*?)">.*?<h3 class="latest-stories__item-headline">(.*?)<\/h3>.*?<time class="latest-stories__item-timestamp">(.*?)<\/time>/gs;
+
+  let match;
+  while ((match = storyPattern.exec(html)) !== null) {
+    const link = "https://time.com" + match[1];
+    const title = match[2];
+
+    latestStories.push({ title, link });
+  }
+
+  return latestStories;
+};
+
+const server = http.createServer(async (req, res) => {
   res.setHeader("Content-Type", "application/json");
 
   if (req.url === "/getTimeStories" && req.method === "GET") {
-    fs.readFile("data.json", "utf8", (err, data) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end(JSON.stringify({ error: "Internal Server Error" }));
-        return;
-      }
-
-      const jsonData = JSON.parse(data);
+    try {
+      const latestStories = await fetchLatestStories();
+      const jsonData = JSON.stringify(latestStories, null, 2);
       res.statusCode = 200;
-      res.end(JSON.stringify(jsonData));
-    });
+      res.end(jsonData);
+    } catch (error) {
+      res.statusCode = 500;
+      res.end(JSON.stringify({ error: "Internal Server Error" }));
+    }
   } else {
     res.statusCode = 404;
     res.end(JSON.stringify({ error: "Not Found" }));
